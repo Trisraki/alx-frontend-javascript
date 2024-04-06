8d0100c5372084cdd01610fcb73e6a8d7af5e2b7
tris-raki <trisraquel1@gmail.com>
2024-04-06 06:36:29 -0700

    Const or let?  Block Scope Arrow functions Parameter defaults Rest parameter syntax for functions The wonders of spread syntax Take advantage of template literals Object property value shorthand syntax No need to create empty objects before adding in properties ES6 method properties For...of Loops Iterator Let's create a report object Iterating through report objects Iterate through object

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const __array = []
+  for (const value of array) {
+    __array.push(appendString + value);
+  }
+
+  return __array;
+}
